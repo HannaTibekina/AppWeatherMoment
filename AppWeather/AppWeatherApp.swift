6319c394d456eb76c7dfeb0f700f6d11557b1b54
gilbert <annatibekina3@gmail.com>
2022-08-05 21:15:37 +0300

    Initial Commit

---
+++ AppWeather/AppWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppWeatherApp.swift
+//  AppWeather
+//
+//  Created by Анна Тибекина on 05.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
